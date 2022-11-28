e9a28e0d4b627e54692d9443889808175c8c41da
BRUHITsABunny <53124399+BRUHItsABunny@users.noreply.github.com>
2022-11-28 12:46:23 +0100

    Add src and clean

--- ansi.go
+++ ansi.go
@@ -2,15 +2,9 @@ package bunterm
 
 import "fmt"
 
-// SRC: // https://www.lihaoyi.com/post/BuildyourownCommandLinewithANSIescapecodes.html
+// SRC: https://www.lihaoyi.com/post/BuildyourownCommandLinewithANSIescapecodes.html
+// SRC: https://gist.github.com/fnky/458719343aabd01cfb17a3a4f7296797
 const esc = "\u001b"
-
-// TODO: Maybe ANSI codes don't always work, find edge cases and make workarounds
-
-// ansiClearTerminalAlternate didn't work on Debian (5.16.18-1)
-func ansiClearTerminalAlternate() string {
-	return fmt.Sprintf("%s[K", esc)
-}
 
 func ansiClearTerminal() string {
 	return fmt.Sprintf("%s[2J", esc)
@@ -22,8 +16,4 @@ func ansiClearLine() string {
 
 func ansiMoveCursor(x, y int16) string {
 	return fmt.Sprintf("%s[%d;%dH", esc, y, x)
-}
-
-func ansiMoveCursorAlternate(x, y int16) string {
-	return fmt.Sprintf("%s[%d;%df", esc, y, x)
 }
